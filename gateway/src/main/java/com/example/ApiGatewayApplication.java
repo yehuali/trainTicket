b4e3b113201a4dc1349ddfb68cbe0ccbe8b7631f
zhangjie <731255090@qq.com>
2019-02-16 17:00:20 +0800

    微服务演示demo

---
+++ gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.example.core.filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * API网关服务
+ * @ EnableZuulProxy 支持网关路由
+ * @ EnableOAuth2Sso 启用OAuth2单点登录
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    /**
+     * 资源过滤器
+     * @return 资源过滤器
+     */
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
+    }
+}
