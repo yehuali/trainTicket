a8cbaa7c947bbfd980a4c283b58bcbd6e3524123
zhangjie <731255090@qq.com>
2019-02-17 16:19:21 +0800

    增加分布式链路调用zipkin

---
+++ sleuth/src/main/java/com/example/ZipkinApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * @EnableZipkinServer 启用Zipkin服务。
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
